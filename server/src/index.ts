24197a4569b2fd0fa9500f218a094b37e98afbe6
Farkhod Khalikov <farkhod.khalikov.main@gmail.com>
2024-12-16 14:13:34 +0500

    Updated Config files for linting

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,8 +4,9 @@ import app from './express'; // Import Express app from express.ts
 // use environment variables
 dotenv.config();
 
-
 // Start the server
 app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT || 5000}`);
+  console.log(
+    `Server is running on http://localhost:${process.env.PORT || 5000}`,
+  );
 });
